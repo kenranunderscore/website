b64ec90299f82c48763d2a4d01298c0b58571508
Johannes Maier <johb.maier@gmail.com>
2020-09-19 23:00:57 +0200

    Use reacl-c

--- src/main/kenranunderscore/app.cljs
+++ src/main/kenranunderscore/app.cljs
@@ -1,5 +1,15 @@
-(ns kenranunderscore.app)
+(ns kenranunderscore.app
+  (:require [reacl-c.core :as c :include-macros true]
+            [reacl-c.browser :as browser]
+            [reacl-c.dom :as dom]))
 
 (defn init
   []
   (println "Hello world"))
+
+(c/def-dynamic main state
+  (c/fragment (dom/div "foo")))
+
+(browser/run (.getElementById js/document "root")
+  main
+  {})
