20a5267fd722e2d6e35a4bb279794da5a07b5283
Johannes Maier <johb.maier@gmail.com>
2020-09-26 23:12:57 +0200

    Add some items

--- src/main/kenranunderscore/app.cljs
+++ src/main/kenranunderscore/app.cljs
@@ -1,15 +1,44 @@
 (ns kenranunderscore.app
   (:require [reacl-c.core :as c :include-macros true]
             [reacl-c.browser :as browser]
+            [active.clojure.functions :as f]
+            [active.clojure.lens :as lens]
             [reacl-c.dom :as dom]))
 
-(defn init
-  []
-  (println "Hello world"))
+(def indices (range 9))
 
-(c/def-dynamic main state
-  (c/fragment (dom/div "foo")))
+(defrecord Row [index cells])
+
+(defrecord Cell [column state])
+
+(defn make-row
+  [i]
+  (->> indices
+       (map (fn [j] (->Cell j {})))
+       (->Row i)))
+
+(def initial-sudoku-state
+  (map make-row indices))
+
+(c/def-item cell
+  (c/with-state-as state
+    (print (pr-str state))
+    (dom/td (str (:state state)))))
+
+(c/def-item sudoku
+  (c/with-state-as state
+    (c/fragment
+     (apply dom/table
+            (map (fn [i]
+                   (apply dom/tr
+                          (map (fn [j]
+                                 (c/focus (lens/>> (lens/at-index i)
+                                                   :cells
+                                                   (lens/at-index j))
+                                          cell))
+                               indices)))
+                 indices)))))
 
 (browser/run (.getElementById js/document "root")
-  main
-  {})
+  sudoku
+  initial-sudoku-state)
