2a747c681f78b97773f0c29c70278ca8b3b6e3e2
Johannes Maier <johb.maier@gmail.com>
2020-09-27 15:33:09 +0200

    Use Halogen

--- src/Main.purs
+++ src/Main.purs
@@ -2,9 +2,30 @@ module Main where
 
 import Prelude
 
+import Data.Maybe (Maybe (..))
 import Effect (Effect)
-import Effect.Console (log)
+import Halogen as Halogen
+import Halogen.Aff as Aff
+import Halogen.HTML as Html
+import Halogen.HTML.Events as Events
+import Halogen.VDom.Driver (runUI)
 
 main :: Effect Unit
-main = do
-  log "🍝"
+main = Aff.runHalogenAff do
+  body <- Aff.awaitBody
+  runUI component unit body
+
+data Action = Hello
+
+component =
+  Halogen.mkComponent
+  { initialState
+  , render
+  , eval : Halogen.mkEval (Halogen.defaultEval { handleAction = handleAction })
+  }
+  where
+    initialState _ = "foo"
+    render state =
+      Html.button [ Events.onClick \_ -> Just Hello ] [ Html.text state ]
+    handleAction = case _ of
+      Hello -> Halogen.modify_ \state -> "Hello!"
