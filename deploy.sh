cd5fd228ecfa7dfbf41724f1d19bffcfc459ec96
Johannes Maier <johannes.maier@mailbox.org>
2022-04-01 22:00:40 +0200

    Build and deploy locally; zap flake
    Reasons:
    
    - The flake made the deployment process more difficult due to having
      to work around the read-only property of the Nix store.
    
    - I want to also render my Emacs configuration, but that's neither
      part of the repo nor should it be a flake input -- I'd have to
      update the website repo on every Emacs config commit.

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
 cd $(dirname "$0")
-nix build
-scp -r result/site/* sync:/var/www/kenran.info
-ssh -t sync "sudo chown -R kenran /var/www/kenran.info && sudo chmod 777 -R /var/www/kenran.info/*"
+emacs -q --script publish.el
+scp -r html/* sync:/var/www/kenran.info
