a3f742de1374e556d76af966002d14da082c56bd
Johannes Maier <johannes.maier@mailbox.org>
2023-03-04 06:13:18 +0100

    Adapt path to emacs config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-cd $(dirname "$0")
-cp ~/dotfiles/modules/programs/emacs/emacs.d/config.org emacs-config/
-emacs -q --script publish.el
+set -e
+
+cd "$(dirname "$0")"
+cp ~/dotfiles/home-manager-modules/emacs/emacs.d/config.org emacs-config/
+emacs --script publish.el
 scp -r html/* sync:/var/www/kenran.info
