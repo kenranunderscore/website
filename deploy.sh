49ae654bd0c5041553fbe8d202f58c6e49cc0c70
Johannes Maier <johannes.maier@mailbox.org>
2023-03-30 07:57:57 +0200

    Also export local package sources

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 set -e
 
 cd "$(dirname "$0")"
+mkdir -p emacs-config/my-packages
 cp ~/dotfiles/home-manager-modules/emacs/emacs.d/config.org emacs-config/
+cp ~/dotfiles/home-manager-modules/emacs/emacs.d/my-packages/*.org emacs-config/my-packages/
 emacs --script publish.el
 scp -r html/* sync:/var/www/kenran.info
