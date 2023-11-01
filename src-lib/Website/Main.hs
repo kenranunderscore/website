538e89759937cefd31c8cbae1fd0714e954b9888
Johannes Maier <johannes.maier@mailbox.org>
2023-11-01 14:36:41 +0100

    Add htmx example

--- src-lib/Website/Main.hs
+++ src-lib/Website/Main.hs
@@ -7,10 +7,14 @@ import Servant
 import Servant.API.Generic
 import Servant.HTML.Lucid (HTML)
 import Lucid qualified as L
+import Lucid.Base qualified as L
 import Network.Wai.Handler.Warp qualified as Warp
 
+type Html = L.Html ()
+
 data Routes' mode = MkRoutes
-  { getIndex :: mode :- Get '[HTML] (L.Html ())
+  { getIndex :: mode :- Get '[HTML] Html
+  , foobar :: mode :- "foobar" :> Get '[HTML] Html
   , static :: mode :- "resources" :> Raw
   }
   deriving stock (Generic)
@@ -22,9 +26,20 @@ main = do
   Warp.run 8080 $ serve (Proxy @Routes) server
   where
     server = MkRoutes { getIndex = getIndexHandler
+                      , foobar = pure $ L.p_ "HI NOW"
                       , static = serveDirectoryWebApp "./resources" }
 
-getIndexHandler :: Handler (L.Html ())
+hxGet = L.makeAttribute "hx-get"
+hxTrigger = L.makeAttribute "hx-trigger"
+hxTarget = L.makeAttribute "hx-target"
+hxSwap = L.makeAttribute "hx-swap"
+
+getIndexHandler :: Handler Html
 getIndexHandler = pure $ do
-  L.head_ $ L.script_ [L.src_ "./resources/htmx.min.js"] (mempty :: String)
-  L.body_ $ L.p_ "Lucid with htmx, fuck yeah"
+  L.head_ $ L.script_ [L.src_ "./resources/htmx.min.js"] (mempty :: Html)
+  L.body_ $ do
+    L.p_ "Lucid with htmx, fuck yeah"
+    L.div_ [L.id_ targetId] "We should swap this out when clicking the button below"
+    L.button_ [hxGet "/foobar", hxTarget ("#" <> targetId), hxSwap "innerHTML", hxTrigger "click"] "click me!"
+  where
+    targetId = "the-target"
